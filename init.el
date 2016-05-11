60e06733c9b37b29d2f29fe99a37956afca4977e
Eric Lynema <eric@lynema.tk>
2016-05-11 10:14:24 -0700

    fixed typo

--- init.el
+++ init.el
@@ -11,10 +11,10 @@
  '(custom-enabled-themes (quote (epl692-1)))
  '(custom-safe-themes
    (quote
-    ("bb9cc2fce236c42aeaf2e9409fcc8a346f05222557e6ba38b3656b585d66fe83" default))))
+    ("bb9cc2fce236c42aeaf2e9409fcc8a346f05222557e6ba38b3656b585d66fe83" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ ))
